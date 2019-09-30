24baffcbcaa5160e70eb97c2f78ef20dcafe027b
AshishTaduce <ashishteddy2@gmail.com>
2019-09-30 15:13:57 +0530

    Exercise One without Strecth

--- lib/main.dart
+++ lib/main.dart
@@ -9,5 +9,44 @@ void main() {
   Text textWidget = Text(msg);
   Center center = Center(child: textWidget);
   MaterialApp myApp = MaterialApp(home: center);
-  runApp(myApp);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+
+          title: Center(
+            child: Text('I Love Flutter'),
+          ),
+          backgroundColor: Colors.lightBlue,
+
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+          leading: Image(
+            image: NetworkImage ('https://mclarencollege.in/images/icon.png'),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg'
+            ),
+          ),
+
+        ),
+        /*floatingActionButton: FloatingActionButton(
+          child: Image (
+              image : NetworkImage ('https://mclarencollege.in/images/icon.png'
+          ),
+          ),
+          onPressed: () {
+            print("Ta TA Ta TA!");
+          },
+        ),*/
+      ),
+    ),
+  );
 }
